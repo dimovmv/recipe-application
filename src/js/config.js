c4809e46abc0c1fd79dbebddf83a577953c6f64d
dimovmv <dimodimov.mv@gmail.com>
2022-05-27 12:03:14 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = "31b15b27-d0c5-4b0f-ace0-90067243f267";
+export const MODAL_CLOSE_SEC = 2.5;
